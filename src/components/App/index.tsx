609bd9c411ebf001848dd95931c73dd1b16a02eb
Tzu Yin <tzyn.wang@gmail.com>
2022-08-21 16:36:17 +0800

    [feat] add Slider

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,13 @@
 import React, { memo, useState } from 'react';
 import classNames from 'classnames';
-import HomeMadeDialog from '@Components/Common/HomeMadeDialog';
+import Dialog from '@Components/Common/Dialog';
+import Slider from '@Components/Common/Slider';
 import scopedStyle from './index.module.css';
 
 function App(): React.ReactElement {
   /* States */
   const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+  const [sliderOpen, setSliderOpen] = useState<boolean>(false);
 
   /* Main */
   return (
@@ -13,12 +15,19 @@ function App(): React.ReactElement {
       <button type="button" onClick={() => setDialogOpen(true)}>
         open dialog
       </button>
-      <HomeMadeDialog
-        dialogOpen={dialogOpen}
-        onDialogClose={() => setDialogOpen(false)}
+      <button type="button" onClick={() => setSliderOpen(true)}>
+        open slider
+      </button>
+      <Dialog
+        open={dialogOpen}
+        onClose={() => setDialogOpen(false)}
+        classes={{ dialog: scopedStyle.biggerDialog }}
       >
         <div>here is dialog's content</div>
-      </HomeMadeDialog>
+      </Dialog>
+      <Slider open={sliderOpen} onClose={() => setSliderOpen(false)}>
+        <div>this is slider's content</div>
+      </Slider>
     </div>
   );
 }
